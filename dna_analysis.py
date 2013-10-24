f6fac2af22e2cc06a0334dd372fe3c63753b3516
rawjor18 <petersonrawls@gmail.com>
2013-10-24 11:01:29 -0700

    data

--- dna_analysis.py
+++ dna_analysis.py
@@ -1,5 +1,5 @@
-# Name: ...
-# Evergreen Login: ...
+# Name: Jordan Rawls
+# Evergreen Login: rawjor18
 # Computer Science Foundations
 # Programming as a Way of Life
 # Homework 3: DNA analysis (Part 1)
