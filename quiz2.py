97936cec4504b27da83cc7f6af8caf99ce312292
rawjor18 <petersonrawls@gmail.com>
2013-10-31 11:43:38 -0700

    quiz2

---
+++ quiz2.py
@@ -0,0 +1,14 @@
+# from a module called lab2
+# import a variale called n
+#print on eof the following things:
+# "greater than 50"
+# "equal to 50"
+# based on the value of n
+ 
+import lab2
+if lab2.n < 50:
+    print 'less than 50'
+if lab2.n > 50:
+    print 'greater than 50'
+if lab2.n == 50:
+    print 'equal to 50'
